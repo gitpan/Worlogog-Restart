eb17ad1ae0eeeb2f40ee1b10da05ccb5828e3a8d
Lukas Mai <mauke@cpan.org>
2013-01-04 03:11:42 +0000

    Import of MAUKE/Worlogog-Restart-0.01 from CPAN.
    gitpan-cpan-distribution: Worlogog-Restart
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MAUKE/Worlogog-Restart-0.01.tar.gz
    gitpan-cpan-author:       MAUKE
    gitpan-cpan-maturity:     released

---
+++ lib/Worlogog/Restart/Restart.pm
@@ -0,0 +1,42 @@
+package Worlogog::Restart::Restart;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+use Moo;
+
+for (qw(name code)) {
+	has $_ => (
+		is => 'ro',
+	);
+}
+
+'ok'
+
+__END__
+
+=head1 NAME
+
+Worlogog::Restart::Restart - restart objects (internal class)
+
+=head1 DESCRIPTION
+
+See L<C<find> in C<Worlogog::Restart>|Worlogog::Restart/find-RESTART>.
+
+=head1 AUTHOR
+
+Lukas Mai, C<< <l.mai at web.de> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Lukas Mai.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
